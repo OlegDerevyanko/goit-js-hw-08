b870b7c19bfad8519df990de3d172911ff7711b0
OlegDerevyanko <106957951+OlegDerevyanko@users.noreply.github.com>
2022-10-31 20:46:40 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,32 +1,32 @@
 import throttle from 'lodash.throttle';
 
 
-const iframe = document.querySelector('iframe');
-const LOCALSTORAGE_KEY = "videoplayer-current-time";
-const player = new Vimeo.Player(iframe);
-
-
-player.on('timeupdate', throttle(onPlay, 1000));
-function onPlay(event) {
-  localStorage.setItem(LOCALSTORAGE_KEY, event.seconds);
-};
-
-const getLocalStorageValue = localStorage.getItem(LOCALSTORAGE_KEY);
-
-if (getLocalStorageValue === null || getLocalStorageValue === undefined) {
-  return;
-} else {
-  player.setCurrentTime(getLocalStorageValue);
-};
-
 // const iframe = document.querySelector('iframe');
+// const LOCALSTORAGE_KEY = "videoplayer-current-time";
 // const player = new Vimeo.Player(iframe);
 
+
 // player.on('timeupdate', throttle(onPlay, 1000));
+// function onPlay(event) {
+//   localStorage.setItem(LOCALSTORAGE_KEY, event.seconds);
+// };
 
-// function onPlay({ seconds }) {
-//   localStorage.setItem('videoplayer-current-time', seconds);
-// }
+// const getLocalStorageValue = localStorage.getItem(LOCALSTORAGE_KEY);
 
-// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+// if (getLocalStorageValue === null || getLocalStorageValue === undefined) {
+//   return;
+// } else {
+//   player.setCurrentTime(getLocalStorageValue);
+// };
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
